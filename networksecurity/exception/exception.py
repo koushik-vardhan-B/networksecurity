cd11fee50acd5bc8c9aa8720ffb0e02529d5c984
koushik-vardhan-B <bandikoushik781@gmail.com>
2025-06-11 15:56:56 +0530

    setting up mongodb

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -9,7 +9,7 @@ class NetworkSecurityException(Exception):
         _, _, exc_tb = error_details.exc_info()
 
         self.lineno = exc_tb.tb_lineno
-        self.file_name = exc_tb.tb_frame.f_code.co_filename
+        self.filename = exc_tb.tb_frame.f_code.co_filename
 
     def __str__(self):
         return f"Error occurred in script: [{self.filename}] at line number: [{self.lineno}] with message: [{self.error_message}]"
